c520cd7b407a9989da269770587a8dc6f041fb6d
seyMour <120465271@qq.com>
2016-03-25 15:32:19 +0800

    hghhg

--- MainActivity.java
+++ MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("s");
+        System.out.println("two");
     }
 
     @Override
