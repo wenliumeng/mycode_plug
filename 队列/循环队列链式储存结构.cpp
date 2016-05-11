cda0b1d379e8214d20a3daae4ba9e2cee8831b34
seyMour <120465271@qq.com>
2016-05-11 17:33:41 +0800

    循环队列链式存储

---
+++ 队列/循环队列链式储存结构.cpp
@@ -0,0 +1,48 @@
+#define MAXSIZE 5
+#define OK 1
+#define ERROR 0
+#include<stdio.h>
+#include<malloc.h>
+#include<stlib.h>
+
+typedef int QElemType;
+
+typedef int Statue;
+
+typedef struct QNode
+{
+	QElemType data;
+	struct QNode *next;
+}QNode,*QueuePtr;
+
+typedef struct LinkQueue {
+	QueuePtr front, rear;
+}LinkQueue;
+
+//入队操作
+Statue EnQueue(LinkQueue *Q, QElemType e)
+{
+	QueuePtr s = (QueuePtr)malloc(sizeof(QNode));
+	if (!s)
+		exit(OVERFLOW);
+	s->data = e;
+	s->next = NULL;
+	Q->rear->next = s;
+	Q->rear = s;
+	return OK;
+}
+
+//出队操作
+Statue OuQueue(LinkQueue *Q, QElemType *e)
+{
+	QueuePtr p;
+	if (Q->front == Q->rear)
+		return ERROR;
+	p = Q->front->next;
+	*e = p->data;
+	Q->front->next = p->next;
+	if (Q->rear == p)
+		Q->rear = Q->front;
+	free(p);
+	return OK;
+}
