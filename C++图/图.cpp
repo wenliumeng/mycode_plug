4ea27c463cc1c912cba1bba6f617d33ab2c04f5c
seyMour <120465271@qq.com>
2016-05-17 16:38:57 +0800

    C++无向图

---
+++ C++图/图.cpp
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<malloc.h>
+typedef char VertexType;        //顶点类型
+typedef int EdgeType;           //边上的权值
+#define MAXVEX 100
+#define INFINITY 65535
+
+typedef struct VertexNode       //顶点表结点
+{
+	VertexType data;
+	EdgeNode *firstnode;
+}VertexNode, AdjList[MAXVEX];
+
+typedef struct EdgeNode {       //边表节点
+	int adjvex;                 //邻接点域，存储该顶点对应的下标
+	EdgeType wight;			    //储存权值
+	struct EdgeNode *next;      //链域，指向下一个邻接点
+}EdgeNode;
+
+typedef struct
+{
+	AdjList adjList;             //表顶点
+	int numVertexes, numEdges;   //图中当前顶点数和边数
+}GraphAdjList;
+
+void CreateALGraph(GraphAdjList *G)
+{
+	int i, j, k;
+	EdgeNode *e;
+	printf("请输入顶点数和边数：\n");
+	scanf("%d,%d",&G->numVertexes,&G->numEdges);
+	for (i = 0; i < G->numVertexes; i++) {
+		scanf(&G->adjList[i].data);                 //输入顶点信息
+		G->adjList[i].firstnode = NULL;
+	}
+
+	for (k = 0; k < G->numEdges; k++) {
+		printf("输入边（vi,vj）上的顶点序号：\n");
+		scanf("%d,%d",&i,&j);
+		/* 头插法开始 */
+		e = (EdgeNode*)malloc(sizeof(EdgeNode));
+		
+		e->adjvex = j;
+		e->next = G->adjList[i].firstnode;
+		G->adjList[i].firstnode = e;
+
+		e = (EdgeNode*)malloc(sizeof(EdgeNode));
+
+		e->adjvex = i;
+		e->next = G->adjList[i].firstnode;
+		G->adjList[j].firstnode = e;
+		/* 头插法结束 */
+	}
+}
