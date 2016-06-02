0faa65778ca6ecc187992dc25dc70bed3d0c0c3b
seyMour <120465271@qq.com>
2016-06-02 11:25:32 +0800

    update

--- 冒泡排序/C++冒泡排序.cpp
+++ 冒泡排序/C++冒泡排序.cpp
@@ -1,5 +1,10 @@
 #include<stdio.h>
 #define MAXSIZE 10
+
+typedef bool Status;
+#define TRUE true
+#define FALSE false
+
 typedef struct
 {
 	int r[MAXSIZE + 1];
@@ -8,8 +13,11 @@ typedef struct
 
 void BubbleSort(SqList *L);
 void BubbleSort1(SqList *L);
-void main(){
-	SqList *l = new SqList;
+void main() {
+	SqList* l = new SqList;
+	SqList L1 = { 9,1,5,8,3,7,4,6,2,NULL,NULL,9};
+	SqList *l2 = &L1;
+	printf("%d", l2->r[3]);
 	l->r[1] = { 9 };
 	l->r[2] = { 1 };
 	l->r[3] = { 5 };
@@ -20,14 +28,14 @@ void main(){
 	l->r[8] = { 6 };
 	l->r[9] = { 2 };
 	l->length = 9;
-	for (int i = 1; i <= l->length; i++){
-		printf("%d,",l->r[i]);
+	/*for (int i = 1; i <= l->length; i++) {
+		printf("%d,", l->r[i]);
 	}
 	printf("\n");
 	BubbleSort1(l);
-	for (int i = 1; i <= l->length; i++){
+	for (int i = 1; i <= l->length; i++) {
 		printf("%d,", l->r[i]);
-	}
+	}*/
 	getchar();
 }
 
@@ -38,28 +46,30 @@ void swap(SqList *L, int i, int j)
 	L->r[j] = temp;
 }
 
-void BubbleSort(SqList *L){
+void BubbleSort(SqList *L) {
 	int i, j;
-	for (i = 1; i < L->length;i++){
-		for (j = L->length - 1; j >= i; j--){
-			if (L->r[j] > L->r[j + 1]){
+	Status flag = TRUE;
+	for (i = 1; i < L->length && flag; i++) {
+		flag = FALSE;
+		for (j = L->length - 1; j >= i; j--) {
+			if (L->r[j] > L->r[j + 1]) {
 				swap(L, j, j + 1);
+				flag = TRUE;
 			}
 		}
 	}
 }
 
-void BubbleSort1(SqList *L){
-	int i, j;
+void BubbleSort1(SqList *L) {
 	int flag = 1;
-	for (int i = 1; i < L->length;i++){
+	for (int i = 1; i < L->length; i++) {
 		flag = flag + 1;
-		for (int j = 1; j <= L->length-i; j++){
-			if (L->r[j] > L->r[j + 1]){
+		for (int j = 1; j <= L->length - i; j++) {
+			if (L->r[j] > L->r[j + 1]) {
 				swap(L, j, j + 1);
 			}
-			if (flag == 344){
-				for (int k = 1; k <= L->length; k++){
+			if (flag == 344) {
+				for (int k = 1; k <= L->length; k++) {
 					printf("%d,", L->r[k]);
 				}
 				printf("\n");
