f2f6b06f57e15123642b7eea88f5a096f61e7c68
seymour <120465271@qq.com>
2016-07-29 00:07:41 +0800

    增加map，和变为线程安全

--- java-experiments/threadsafe.java
+++ java-experiments/threadsafe.java
@@ -16,7 +16,7 @@ public class threadsafe {
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    list.add(finalI);
+                    list.add(finalI);             
                 }
             }).start();
         }
@@ -33,6 +33,13 @@ public class threadsafe {
     public static void main(String[] args) throws InterruptedException {
         List list = new ArrayList();
         List lists = Collections.synchronizedList(new ArrayList());
+		
+		//Map map = new HashMap<>();
+		//方法中用synchronized (map){
+        //                map.add(finalI)
+        //            }
+		//就能使list或map线程安全
+		
         //里面的数据都是乱序的，无论线程安全与否，线程安全只是保证所有线程都实际执行了正确的插入操作
         System.out.println(demo(list)+"/"+demo(lists));
     }
